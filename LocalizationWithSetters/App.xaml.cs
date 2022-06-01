d40ddadb5f9850f354f9983842669ea8ca6ae6ae
Niklas Möller <mollern@net.chalmers.se>
2022-06-01 12:19:50 +0200

    First commit

---
+++ LocalizationWithSetters/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFLocalizeExtension.Engine;
+
+namespace LocalizationWithSetters
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            LocalizeDictionary.Instance.SetCurrentThreadCulture = true;
+            //Sets the default language on startup
+            LocalizeDictionary.Instance.Culture = new System.Globalization.CultureInfo("en-US");
+        }
+    }   
+
+}
---
+++ LocalizationWithSetters/Page1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace LocalizationWithSetters
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public static XmlDataProvider selectedConfigTypeXMLDataProvider { get; set; }
+
+        public Page1()
+        {
+            InitializeComponent();
+
+            selectedConfigTypeXMLDataProvider = new XmlDataProvider();
+
+            XmlDocument config = new XmlDocument();
+            
+            //Setting the PatientType to PLP
+            string configXmlAsString =
+                "<?xml version=\"1.0\" encoding=\"utf-8\" ?>" +
+                "<configurations>" +
+                "<PatientType>PLP</PatientType>" +
+                "</configurations>";
+
+            config.LoadXml(configXmlAsString);
+
+            selectedConfigTypeXMLDataProvider.Document = config;
+
+            //For binding to the XAML code
+            this.DataContext = selectedConfigTypeXMLDataProvider;
+
+        }
+    }
+}
